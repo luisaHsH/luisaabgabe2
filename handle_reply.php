0f140e549168fa6f6b2dfff38cb73a89f22da557
luisaHsH <luisa_rabbe@web.de>
2026-01-23 12:29:18 +0100

    handle_reply.php

--- handle_reply.php
+++ handle_reply.php
@@ -2,16 +2,14 @@
 require_once 'includes/CommentManager.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["absenden"])) {
-    if (!empty($_POST["name"]) && !empty($_POST["kommentar"]) && !empty($_POST["seite"])) {
-        $name = htmlspecialchars(trim($_POST["name"]));
-        $kommentar = htmlspecialchars(trim($_POST["kommentar"]));
-        $parent_id = htmlspecialchars(trim($_POST["parent_id"]));
-
-        $seite = htmlspecialchars(trim($_POST["seite"]));
+    if (!empty($_POST["name"]) && !empty($_POST["kommentar"]) && !empty($_POST["parent_id"])) {
+        $name = trim($_POST["name"]);
+        $kommentar = trim($_POST["kommentar"]);
+        $parent_id = (int)$_POST["parent_id"];
+        $seite = trim($_POST["seite"]);
 
         $commentManager = new CommentManager();
-        $commentManager->saveReply($name, $kommentar,$parent_id);
-
+        $commentManager->saveReply($name, $kommentar, $parent_id);
 
         // Zurück zur ursprünglichen Seite
         header("Location: " . $seite);
