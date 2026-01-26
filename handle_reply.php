284b271cb66887047c88c03da43fe2c63aa8cf88
luisaHsH <luisa_rabbe@web.de>
2026-01-26 13:04:07 +0100

    Update handle_reply.php

--- handle_reply.php
+++ handle_reply.php
@@ -1,28 +1,7 @@
 <?php
-declare(strict_types=1);
+require_once 'includes/CommentManager.php';
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-require_once __DIR__ . '/includes/CommentManager.php';
 $cm = new CommentManager();
+$cm->addReply((int)$_POST['comment_id'], $_POST['name'], $_POST['antwort']);
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-  header('Location: index.php');
-  exit;
-}
-
-$seite = basename((string)($_POST['seite'] ?? 'index.php'));
-$kommentarId = (int)($_POST['kommentar_id'] ?? 0);
-$name = trim((string)($_POST['name'] ?? ''));
-$antwort = trim((string)($_POST['antwort'] ?? ''));
-
-if ($kommentarId <= 0 || $name === '' || $antwort === '') {
-  header('Location: ' . $seite . '?err=1#comments');
-  exit;
-}
-
-$cm->addReply($kommentarId, $name, $antwort);
-
-header('Location: ' . $seite . '#comments');
-exit;
+header("Location: " . $_POST['seite'] . "#comments");
