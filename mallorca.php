5bb960c6651fa1c03e886c09bf6e2abb124056fb
luisaHsH <luisa_rabbe@web.de>
2026-01-26 12:35:42 +0100

    mallorca.php

--- mallorca.php
+++ mallorca.php
@@ -68,9 +68,10 @@
 
     </div>
 
-    <?php include 'includes/comment_section.php'; ?>
+    <?php include __DIR__ . '/includes/comment_section.php'; ?>
     <?php include 'includes/comments_display.php'; ?>
     <?php include 'includes/footer.php'; ?>
 </body>
+
 
 </html>
